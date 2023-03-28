2601052389026e1acd17e80932f6a69921f61a52
alfaradito <radito.alfa@gmail.com>
2023-03-28 20:01:08 +0700

    add learn_23.php

--- learn_2.php
+++ learn_2.php
@@ -1,4 +1,5 @@
 <?php
     echo("Ini diketik ALFA\n");
     echo("Klo yang ini diketik OZART\n");
+    echo("Ini diketik ALFA lagi\n");
 ?>
---
+++ learn_23.php
@@ -0,0 +1,3 @@
+<?php
+    echo("KIRA Service.. onward");
+?>
